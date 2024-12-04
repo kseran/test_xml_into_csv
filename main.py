7b0fed2eda3c8e738542629065ea3d9154b6d239
kseran <mr.lord.moon@mail.ru>
2024-12-04 22:51:39 +0300

    half work version

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import os
 import shutil
 import sys
+from datetime import datetime
 from xml.etree import ElementTree
 import csv
 
@@ -14,9 +15,28 @@ logging.basicConfig(
 )
 
 
-def xml_into_csv(file_path: str) -> None:
+def is_valid_csv_amount(amount_str):
+    """Check that the amount is a positive number with two decimal places."""
+    try:
+        amount = float(amount_str)
+        if amount < 0 or not (amount_str.endswith('.00') or amount_str.endswith('.01') or amount_str.endswith('.99')):
+            return False
+        return True
+    except ValueError:
+        return False
+
+
+def is_valid_period(period_str):
+    """Check the period format MMYYYY."""
+    if len(period_str) == 6 and period_str.isdigit():
+        return True
+    return False
+
+
+def xml_into_csv(file_path: str) -> None: #TODO пройтись по тз ещё раз и доделать
     if not file_path.lower().endswith(".xml"):
         logging.error(f"Файл {file_path} не является xml файлом. Обработка не возможна")
+        os.makedirs('bad', exist_ok=True)
         shutil.move(file_path, os.path.join('bad', os.path.basename(file_path)))
         return
     # Чтение XML из файла
@@ -24,23 +44,60 @@ def xml_into_csv(file_path: str) -> None:
     root = tree.getroot()
     encoding_file = get_xml_encoding(file_path=file_path)
 
-    # Получение текста из XML
-    text = ElementTree.tostring(root, encoding='unicode')
-
-    # Преобразование текста в кодировку Windows-1251
-    encoded_text = text.encode(encoding_file)
-
-    # Вывод закодированного текста
-    print(encoded_text.decode(encoding_file))  # Декодируем для корректного вывода в терминал
-
-    with open(file_path.replace('.xml', '.csv'), 'a', newline='',
-              encoding='utf=8') as f:  # здесь записываем данные в конечный csv
-        writer = csv.writer(f, delimiter=';')
-
-
     # отправлем файл в архив
-    shutil.move(file_path, os.path.join('arh', os.path.basename(file_path)))
+    os.makedirs('arh', exist_ok=True)
+    # shutil.move(file_path, os.path.join('arh', os.path.basename(file_path)))
     logging.info(f"Файл {file_path} успешно обработан.")
+
+    with open(file_path.replace('.xml' and '.XML', '.csv'), 'a', newline='',
+              encoding=encoding_file) as f:  # здесь записываем данные в конечный csv
+        writer = csv.writer(f, delimiter=';')
+        unique_records = set()
+        payer_info = root[1]
+        for record in payer_info:
+            print(record.find('ЛицСч'))
+            account_number = record.find('ЛицСч')
+            full_name = record.find('ФИО')
+            address = record.find('Адрес')
+            period = record.find('Период')
+            amount = record.find('Сумма')
+
+            # If key attributes are missing
+            if account_number is None or period is None:
+                logging.warning(f"Row number  is missing one of the key attributes.")
+                continue
+
+            account_number = account_number.text if account_number is not None else ''
+            full_name = full_name.text if full_name is not None else ''
+            address = address.text if address is not None else ''
+            period = period.text if period is not None else ''
+            amount = amount.text if amount is not None else ''
+
+            if not is_valid_period(period):
+                logging.warning(f"Invalid period format: {period}. Row skipped.")
+
+            if not is_valid_csv_amount(amount):
+                logging.warning(f"Invalid amount format: {amount}. Row skipped.")
+
+            unique_key = (account_number, period)
+            if unique_key in unique_records:
+                logging.warning(
+                    f"Duplicate record: Account Number = {account_number}; Period = {period}. Record skipped.")
+
+            unique_records.add(unique_key)
+
+            validity_date = root[0].find('.//ДатаФайл').text
+            if validity_date is None:
+                validity_date = ''
+            writer.writerow([
+                os.path.basename(file_path),
+                validity_date,
+                account_number,
+                full_name,
+                address,
+                period,
+                amount
+            ])
 
 
 def get_xml_encoding(file_path: str) -> str | None:
@@ -57,13 +114,15 @@ def get_xml_encoding(file_path: str) -> str | None:
 
 
 if __name__ == "__main__":
+    xml_into_csv(file_path="input — копия.XML")  # для тестов, позже удалить
+
     if len(sys.argv) != 2:
         logging.error("Неверная команда запуска. Используйте: python main.py <путь к xml-файлу>")
         sys.exit(1)
 
     file_path = sys.argv[1]
+    print(file_path)
     if not os.path.exists(file_path):
         print("Файл не найден.")
         sys.exit(1)
     xml_into_csv(file_path=file_path)
-
